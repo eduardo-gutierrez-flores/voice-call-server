239f49cc131dee7b003199c14727148608099595
Eduardo Gutierrez Flores <egutierrez@fc-rd.com>
2022-07-19 10:40:22 -0500

    adjustments to dr and salud prox

--- src/index.js
+++ src/index.js
@@ -21,41 +21,41 @@ io.on('connection', (socket) => {
     console.log(`conectado: ${socket.id}`)
 
     socket.on('create_user', (data) => {
-        usersActive.push({name: data.name, id: socket.id});
+        usersActive.push({username: data.username, id: socket.id, name: data.name, role: data.role});
         io.emit('new_active', usersActive)
-        // socket.join(socket.id)
+    })
+    
+    socket.on('check_user', (data, callback) => {
+        const userAvailable = usersActive.find((user) => user.username === data.username && user.role === data.role)
+        callback(userAvailable)
     })
     
     socket.on('create_call', (data) => {
         socket.join(data.room)
-        socket.to(data.id).emit('recive_call', {nameEmit: data.nameEmit, name: data.name, id: data.id, idEmit: socket.id, token: data.token, room: data.room})
+        const name = usersActive.find((user) => user.username === data.name)
+        const nameEmit = usersActive.find((user) => user.username === data.nameEmit)
+        socket.to(data.id).emit('recive_call', {nameEmit: nameEmit.name, name: name.name, id: data.id, idEmit: socket.id, token: data.token, room: data.room})
     })
 
     socket.on('leave_call', (data) => {
-        console.log(data);
         const index = usersInCalls.indexOf(data.id);
         usersInCalls.splice(index, 1);
-        socket.to(data.room).emit('leave', {name: data.name, id: data.id, token: data.token})
+        socket.to(data.room).emit('leave', {username: data.username, id: data.id, token: data.token})
         io.emit('users_in_call', usersInCalls)
         socket.leave(data.room)
-        // socket.join(socket.id)
     })
     
     socket.on('cancel_call', (data) => {
-        socket.to(data.id).emit('leave', {name: data.name, id: data.id, token: data.token})
-        // socket.join(socket.id)
+        socket.to(data.id).emit('leave', {username: data.username, id: data.id, token: data.token})
     })
     
     socket.on('answer_call', (data) => {
-        console.log(data);
         socket.join(data.room)
         usersInCalls.push(data.id, data.idEmit)
         io.emit('users_in_call', usersInCalls)
-        // socket.join(socket.id)
     })
     
     socket.on('disconnect', (data) => {
-        console.log(socket.id, 'disconnected');
         const user = usersActive.find((user) => user.id === socket.id);
         const index = usersActive.indexOf(user);
         usersActive.splice(index, 1);
